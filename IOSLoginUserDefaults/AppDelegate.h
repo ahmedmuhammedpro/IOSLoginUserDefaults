c4cf0267cb26a63a87125ca90903d471028e1380
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-03-18 04:02:00 +0200

    Initial Commit

---
+++ IOSLoginUserDefaults/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSLoginUserDefaults
+//
+//  Created by ahmedpro on 3/18/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
